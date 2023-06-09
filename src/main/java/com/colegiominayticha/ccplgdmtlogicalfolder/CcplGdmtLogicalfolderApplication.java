875ae4db595a8f5171525cf982aa33f8cb134444
pedro-chavezcastro <pedro.chavezcastro@outlook.com>
2023-06-09 18:47:18 -0500

    first commit

---
+++ src/main/java/com/colegiominayticha/ccplgdmtlogicalfolder/CcplGdmtLogicalfolderApplication.java
@@ -0,0 +1,13 @@
+package com.colegiominayticha.ccplgdmtlogicalfolder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcplGdmtLogicalfolderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcplGdmtLogicalfolderApplication.class, args);
+	}
+
+}
